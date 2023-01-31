f7df574b9f6ee5eec8230840ab20583cc3bffeff
goder-0 <goder0@naver.com>
2023-01-31 13:25:09 +0900

    Create 기본수학

--- 문자열/1712.py
+++
@@ -1,7 +0,0 @@
-import sys
-import math
-A,B,C = map(int,sys.stdin.readline().split())
-if C-B>0:
-    print(math.trunc(A/(C-B))+1)
-else:
-    print(-1)
--- 문자열/2292.py
+++
@@ -1,6 +0,0 @@
-n=int(input())
-k=0
-while True:
-    if n>3*k*(k+1)+1:k+=1
-    else: break
-print(k+1)
