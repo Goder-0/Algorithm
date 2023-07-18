d7f71d1e28ba93303ea53779a629da602e0af250
goder-0 <goder0@naver.com>
2023-07-18 17:19:24 +0900

    좌표 정렬하기

---
+++ 11650.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define io ios::sync_with_stdio(false); cin.tie(0);
+
+using namespace std;
+
+
+bool compare(pair<int,int> a,pair<int,int> b){
+    if(a.first==b.first) return a.second<b.second;
+    return a.first<b.first;
+}
+
+int main()
+{
+    io;
+    int N;
+    cin>>N;
+    vector<pair<int,int>> pos(N);
+    for(int i=0;i<N;i++){
+        cin>>pos[i].first>>pos[i].second;
+    }
+    sort(pos.begin(),pos.end(),compare);
+    for(auto i:pos){
+        cout<<i.first<<' '<<i.second<<'\n';
+    }
+    return 0;
+}
