ce6078d2bac15ef83dbd25ff1d8316f0dbb78f6f
goder-0 <goder0@naver.com>
2023-07-21 01:30:19 +0900

    알고리즘 수업 - 너비 우선 탐색 1

---
+++ 그래프와 순회/24444.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <deque>
+
+#define io ios::sync_with_stdio(false); cin.tie(0);
+
+using namespace std;
+
+
+int visited[100001];
+vector<int> graph[100001];
+deque<int> q;
+int N,M,R,seq=0;
+void bfs(int R){
+    for(int i=1;i<N+1;i++){
+        if(i!=R) visited[i]=0;
+    }
+    visited[R]=++seq;
+    q.push_back(R);
+    while(!q.empty()){
+        int u=q.front();
+        q.pop_front();
+        for(int v:graph[u]){
+            if(visited[v]==0){
+                visited[v]=++seq;
+                q.push_back(v);
+            }
+        }
+    }
+}
+
+int main()
+{
+    io;
+    cin>>N>>M>>R;
+    for(int i=0;i<M;i++){
+        int u,v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    for(vector<int> &v:graph){
+        sort(v.begin(),v.end());
+    }
+
+    bfs(R);
+
+    while(!q.empty()){
+        cout<<q.front()<<'\n';
+        q.pop_front();
+    }
+
+    for(int i=1;i<N+1;i++){
+        cout<<visited[i]<<'\n';
+    }
+
+    return 0;
+}
