bb242ecfe2750673137a0036a4d0fe0e7545bf8a
goder <goder0@naver.com>
2024-06-23 10:05:01 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define io ios::sync_with_stdio(0); cin.tie(0);
+
+using namespace std;
+
+int counts[12];
+
+int main()
+{
+    io;
+    int N,K,Y,S,ans=0;
+    cin>>N>>K;
+    for(int i=0;i<N;i++){
+        cin>>S>>Y;
+        counts[Y-1+S*6]++;
+    }
+    for(int i=0;i<12;i++){
+        ans+=(counts[i]+1)/2;
+    }
+    cout<<ans;
+    return 0;
+}
